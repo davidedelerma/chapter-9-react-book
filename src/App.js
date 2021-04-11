e835e5d5b703ae96c39d353edc01f93981ae3ae5
Davide de Lerma <davide.delerma@dorbit.space>
2021-04-11 15:27:17 +0100

    error boundary and suspense

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, Suspense ,lazy } from "react";
+import Agreement from "./Agreement"
+import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader";
+import ErrorBoundary from "./ErrorBoundary";
 
-function App() {
+const Main = lazy(() => import("./Main"));
+
+function ErrorScreen({ error }) {
+  //
+  // Here you can handle or track the error before rendering the message
+  //
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="error">
+      <h3>We are sorry... something went wrong</h3>
+      <p>We cannot process your request at this moment.</p>
+      <p>ERROR: {error.message}</p>
     </div>
   );
+}
+
+function App() {
+  const [agree, setAgree] = useState(false);
+
+  if (!agree)
+    return <Agreement onAgree={() => setAgree(true)} />;
+  return(
+    <ErrorBoundary fallback={ErrorScreen}>
+      <Suspense fallback={<ClimbingBoxLoader />}>
+        <Main />
+      </Suspense>
+    </ErrorBoundary>
+  ); 
 }
 
 export default App;
